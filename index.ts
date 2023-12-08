66239f686b670965acfe382d0357ba7f83ff4d1a
Felipe Parra <fpr29.10@gmail.com>
2023-12-07 18:26:05 -0600

    Fixed docker file

--- index.ts
+++ index.ts
@@ -11,5 +11,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port} - src`);
+  console.log(`Server is running on port ${port}`);
 });
